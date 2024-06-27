4119917ba3637b9adc2f7c6a027e079b087c47cb
acer <spriyak3003@gmail.com>
2024-06-27 11:08:18 +0530

    Changed app name

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:loginchat/login_page.dart';
+import 'package:Vaayusastra/login_page.dart';
 
 void main() {
   testWidgets('LoginPage renders correctly', (WidgetTester tester) async {
